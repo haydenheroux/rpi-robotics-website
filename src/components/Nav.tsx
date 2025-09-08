d0fca69c2130883118b0ec2c5f384f785abef76b
Hayden Heroux <haydenheroux@gmail.com>
2025-09-07 21:53:59 -0400

    Run Prettier.

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -2,9 +2,15 @@ function Nav() {
   return (
     <nav className="h-16 mx-8 my-4 flex flex-row gap-8 justify-start items-center overflow-x-scroll">
       <img src="./icon.svg" alt="RPI Robotics Club Logo" className="h-14" />
-      <span className="type-subhead text-brand-white hover:text-neutral-400">Projects</span>
-      <span className="type-subhead text-brand-white hover:text-neutral-400">Leadership</span>
-      <span className="type-subhead text-brand-white hover:text-neutral-400">Calendar</span>
+      <span className="type-subhead text-brand-white hover:text-neutral-400">
+        Projects
+      </span>
+      <span className="type-subhead text-brand-white hover:text-neutral-400">
+        Leadership
+      </span>
+      <span className="type-subhead text-brand-white hover:text-neutral-400">
+        Calendar
+      </span>
     </nav>
   );
 }
