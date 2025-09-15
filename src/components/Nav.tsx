b283ee91971d337e3319935ac6d41accd2ffd575
Hayden Heroux <haydenheroux@gmail.com>
2025-09-15 15:51:39 -0400

    Fix title width issue.

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -2,7 +2,7 @@ function Nav() {
   return (
     <nav className="w-screen h-16 py-4 px-6 bg-neutral-50 drop-shadow-sm/25 drop-shadow-neutral-500">
       <div className="w-full lg:w-4xl lg:mx-auto flex flex-row gap-8 justify-between items-center">
-        <span className="min-w-26 type-title text-neutral-700">
+        <span className="min-w-32 type-title text-neutral-700">
           RPI Robotics
         </span>
         <div className="px-2 overflow-x-scroll text-ellipsis flex flex-row gap-8 justify-start items-center">
