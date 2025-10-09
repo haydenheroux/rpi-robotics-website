d5f212f5cdedb75c1c3337758591894891ea75e2
Hayden Heroux <haydenheroux@gmail.com>
2025-10-09 14:52:44 -0400

    Fixes for responsive scaling.

--- src/Leadership.tsx
+++ src/Leadership.tsx
@@ -2,7 +2,7 @@ import PersonCard from "./components/PersonCard";
 
 function Leadership() {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 gap-12">
+    <div className="grid grid-cols-1 lg:grid-cols-2 gap-12">
       <PersonCard
         name="Jason Zhang"
         position="President"
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -10,7 +10,7 @@ function Nav() {
   return (
     <>
       <nav
-        className={`w-screen h-16 py-4 px-8 bg-neutral-50 ${!showNav ? "drop-shadow-sm/25 drop-shadow-neutral-500" : "relative z-10"}`}
+        className={`w-screen min-h-16 py-4 px-8 bg-neutral-50 ${!showNav ? "drop-shadow-sm/25 drop-shadow-neutral-500" : "relative z-10"}`}
       >
         <div className="w-full lg:w-4xl lg:mx-auto flex flex-row gap-8 justify-between items-center">
           <Link to="/">
