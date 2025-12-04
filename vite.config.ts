c164dce1a87675936fffbfb185c0dc290081e7ac
Hayden Heroux <haydenheroux@gmail.com>
2025-12-04 12:19:48 -0500

    Static-site generation.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
+import { ssr } from "vite-plugin-ssr/plugin";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), ssr({ prerender: true })],
 });
