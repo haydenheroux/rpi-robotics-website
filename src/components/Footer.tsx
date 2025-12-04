7f155aa5a76e72808a7a05088ecc1561768cb41f
Hayden Heroux <haydenheroux@gmail.com>
2025-12-04 10:48:16 -0500

    Change fonts.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <div className="w-full my-16 flex justify-center">
       <span className="px-4 py-2 flex items-center type-body text-standard bg-standard rounded-2xl">
-        <p className="mr-1">
+        <p className="mr-1.5">
           Created by{" "}
           <a className="link-dotted" href="https://www.github.com/haydenheroux">
             Hayden Heroux
