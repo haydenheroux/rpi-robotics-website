c4cb5cd01e279d82071c8072fb2abc37f6ba1d23
Hayden Heroux <haydenheroux@gmail.com>
2026-01-02 21:58:17 -0500

    Update Leadership labels

--- src/Leadership.tsx
+++ src/Leadership.tsx
@@ -1,12 +1,11 @@
 import PersonCard from "./components/PersonCard";
 import { useContent } from "./contexts/ContentContext";
+import type Person from "./types/Person";
 
-function Leadership() {
-  const { content } = useContent();
-
+function LeadershipList({ people }: { people: Person[] }) {
   return (
     <div className="grid grid-cols-1 lg:grid-cols-2 gap-12">
-      {content.leadership.map((person) => (
+      {people.map((person) => (
         <PersonCard
           name={person.name}
           email={person.email}
@@ -14,6 +13,23 @@ function Leadership() {
           projects={person.projects}
         />
       ))}
+    </div>
+  );
+}
+
+function Leadership() {
+  const { content } = useContent();
+
+  const isTeamLead = (person: Person) => person.position === "Team Lead";
+  const teamLeads = content.leadership.filter(isTeamLead);
+  const eBoard = content.leadership.filter((p) => !isTeamLead(p));
+
+  return (
+    <div className="flex flex-col gap-12">
+      <h1 className="type-title text-standard text-center">Executive Board</h1>
+      <LeadershipList people={eBoard} />
+      <h1 className="type-title text-standard text-center">Team Leads</h1>
+      <LeadershipList people={teamLeads} />
     </div>
   );
 }
